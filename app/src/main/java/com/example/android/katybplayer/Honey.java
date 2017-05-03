ed5ce25420c3626137f0037011a486711048adeb
agnik <reira@wp.pl>
2017-05-03 22:53:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/katybplayer/Honey.java
@@ -0,0 +1,13 @@
+package com.example.android.katybplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Honey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_honey);
+    }
+}
---
+++ app/src/main/java/com/example/android/katybplayer/LittleRed.java
@@ -0,0 +1,13 @@
+package com.example.android.katybplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LittleRed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_little_red);
+    }
+}
